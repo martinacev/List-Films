2397869c017021dd9a4b6b323345b9222149b26e
martinacev <103221910+martinacev@users.noreply.github.com>
2024-12-15 11:35:47 +0100

    fixing-header

--- FilmList/src/components/header/Header.jsx
+++ FilmList/src/components/header/Header.jsx
@@ -1,23 +1,14 @@
-
-import PropTypes from 'prop-types'; 
 import Languages from '../languages/Languages';
 import classes from "./Header.module.css";
 
-const Header = ({ onLanguageChange, title }) => {
+const Header = ({ onLanguageChange}) => {
   return (
     <div className={classes.container}>
-      <h1 className={classes.title}>{title}</h1>
+      <h1 className={classes.title}>Star Wars Films</h1>
       <Languages onLanguageChange={onLanguageChange} /> 
     </div>
   );
 };
 
-Header.defaultProps = {
-  title : "Star Wars Films"
-}
-
-Header.PropTypes = {
-  title: PropTypes.string,
-}
 
 export default Header;
